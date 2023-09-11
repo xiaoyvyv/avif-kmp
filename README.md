a291e38f3fee4ae44665dc7f53411b889caae423
seiko <seiko_des@outlook.com>
2023-09-11 10:07:11 +0800

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Avif-Kmp
+
+ing...
---
+++ library/build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    kotlin("multiplatform")
+    id("com.android.library")
+    id("org.jetbrains.compose")
+}
+
+kotlin {
+    androidTarget()
+    jvm()
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+
+    @Suppress("OPT_IN_USAGE")
+    targetHierarchy.default {
+        group("jvmCommon") {
+            withAndroidTarget()
+            withJvm()
+        }
+    }
+}
+
+android {
+    compileSdk = (findProperty("android.compileSdk") as String).toInt()
+    namespace = "com.seiko.avif"
+    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
+    defaultConfig {
+        minSdk = (findProperty("android.minSdk") as String).toInt()
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlin {
+        jvmToolchain(11)
+    }
+}
