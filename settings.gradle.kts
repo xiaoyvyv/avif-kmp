97d66dba4da84e92511afb796feccddad936f3fa
seiko <seiko_des@outlook.com>
2023-09-11 14:00:55 +0800

    add cklib

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,6 +17,8 @@ pluginManagement {
         val kotlinVersion = extra["kotlin.version"] as String
         val agpVersion = extra["agp.version"] as String
         val composeVersion = extra["compose.version"] as String
+        val cklibVersion = extra["cklib.version"] as String
+
 
         kotlin("jvm").version(kotlinVersion)
         kotlin("multiplatform").version(kotlinVersion)
@@ -26,6 +28,8 @@ pluginManagement {
         id("com.android.library").version(agpVersion)
 
         id("org.jetbrains.compose").version(composeVersion)
+
+        id("co.touchlab.cklib").version(cklibVersion)
     }
 }
 
