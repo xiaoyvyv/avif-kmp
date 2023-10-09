75f880e91127c322cd176512ca1b31a9db1bd034
seiko <seiko_des@outlook.com>
2023-10-09 13:41:44 +0800

    add AvifFrame.createPlatformBitmap

--- library/src/commonMain/kotlin/com/seiko/avif/AvifFrameExt.kt
+++ library/src/commonMain/kotlin/com/seiko/avif/AvifFrameExt.kt
@@ -1,12 +1,13 @@
 package com.seiko.avif
 
-fun AvifFrame.getBitmapResult(
-    width: Int = getWidth(),
-    height: Int = getHeight(),
-): Result<PlatformBitmap> {
+fun AvifFrame.getBitmapResult(): Result<PlatformBitmap> {
     return runCatching {
-        createPlatformBitmap(width, height).also {
+        createPlatformBitmap().also {
             decodeFrame(it)
         }
     }
+}
+
+fun AvifFrame.createPlatformBitmap(): PlatformBitmap {
+    return createPlatformBitmap(getWidth(), getHeight())
 }
