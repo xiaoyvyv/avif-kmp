698ec24ade71df13016e0db438489ff30bb6663f
seiko <seiko_des@outlook.com>
2023-10-09 16:08:24 +0800

    use local yuv for build android

--- library/darwin/scripts/build-android.sh
+++ library/darwin/scripts/build-android.sh
@@ -1,5 +1,52 @@
 cd libavif || exit 255
 
+
+# START libyuv
+if ! [ -f ext/libyuv ]; then
+  git clone --single-branch https://chromium.googlesource.com/libyuv/libyuv ext/libyuv
+fi
+cd ext/libyuv || exit 255
+
+libyuv_build_dir="build/${ABI}"
+rm -rf "${libyuv_build_dir}"
+mkdir -p "${libyuv_build_dir}"
+
+cmake -B "${libyuv_build_dir}" \
+    -DCMAKE_TOOLCHAIN_FILE="${ANDROID_NDK}/build/cmake/android.toolchain.cmake" \
+    -DANDROID_ABI="${ABI}" \
+    -DANDROID_STL=c++_shared \
+    -DANDROID_PLATFORM="android-${ANDROID_MIN_SDK}" \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_SYSTEM_NAME=Android \
+    -DBUILD_STATIC_LIBS=ON
+cmake --build "${libyuv_build_dir}"
+
+cd ../..
+# END libyuv
+
+# START libwebp
+if ! [ -f ext/libwebp ]; then
+  git clone --single-branch https://chromium.googlesource.com/webm/libwebp ext/libwebp
+fi
+cd ext/libwebp || exit 255
+
+libwebp_build_dir="build"
+rm -rf "${libwebp_build_dir}"
+mkdir -p "${libwebp_build_dir}"
+
+cmake -B "${libwebp_build_dir}" \
+    -DCMAKE_TOOLCHAIN_FILE="${ANDROID_NDK}/build/cmake/android.toolchain.cmake" \
+    -DANDROID_ABI="${ABI}" \
+    -DANDROID_STL=c++_shared \
+    -DANDROID_PLATFORM="android-${ANDROID_MIN_SDK}" \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_SYSTEM_NAME=Android \
+    -DBUILD_STATIC_LIBS=ON
+cmake --build "${libwebp_build_dir}"
+
+cd ../..
+# END libwebp
+
 # START dav1d
 if ! [ -f ext/dav1d ]; then
   git clone -b 1.2.1 --depth 1 https://code.videolan.org/videolan/dav1d.git ext/dav1d
@@ -48,16 +95,22 @@ cmake -B "${build_dir}" \
   -DCMAKE_BUILD_TYPE=Release \
   -DBUILD_SHARED_LIBS=OFF \
   -DAVIF_CODEC_DAV1D=ON \
-  -DAVIF_LOCAL_DAV1D=ON
+  -DAVIF_LOCAL_DAV1D=ON \
+  -DAVIF_LOCAL_LIBYUV=ON \
+  -DAVIF_LOCAL_LIBSHARPYUV=ON
 cmake --build "${build_dir}"
 # END avif
 
 # START copy *.a & rm cache dir
 mkdir -p "${ANDROID_OUTPUT_DIR}"
 
+#cp -v ext/libyuv/build/${ABI}/libyuv.a "${ANDROID_OUTPUT_DIR}" || exit 255
+#cp -v ext/libwebp/build/libsharpyuv.a "${ANDROID_OUTPUT_DIR}" || exit 255
 cp -v ext/dav1d/build/${ABI}/src/*.a "${ANDROID_OUTPUT_DIR}" || exit 255
 cp -v ${build_dir}/*.a "${ANDROID_OUTPUT_DIR}" || exit 255
 
+#rm -rf "ext/libyuv/build/${ABI}"
+#rm -rf "ext/libwebp/build"
 rm -rf "ext/dav1d/build/${ABI}"
 rm -rf "${build_dir}"
 # END copy *.a & rm cache dir
