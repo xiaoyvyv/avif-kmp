0584406bd4d2cb7be94710b7eb4f3ba880b84a20
seiko <seiko_des@outlook.com>
2023-10-11 10:24:55 +0800

    update native avif image decode frame

--- library/src/nativeMain/kotlin/com/seiko/avif/AvifFrame.kt
+++ library/src/nativeMain/kotlin/com/seiko/avif/AvifFrame.kt
@@ -7,8 +7,11 @@ import kotlinx.cinterop.memScoped
 import kotlinx.cinterop.pointed
 import kotlinx.cinterop.ptr
 import kotlinx.cinterop.toKString
+import org.jetbrains.skia.ColorType
 import platform.avif.AVIF_RESULT_OK
 import platform.avif.AVIF_RGB_FORMAT_RGBA
+import platform.avif.AVIF_RGB_FORMAT_RGB_565
+import platform.avif.AVIF_TRUE
 import platform.avif.avifImage
 import platform.avif.avifImageYUVToRGB
 import platform.avif.avifRGBImage
@@ -36,11 +39,30 @@ actual class AvifFrame internal constructor(
     }
 
     actual fun decodeFrame(bitmap: PlatformBitmap) = memScoped {
+        if (bitmap.colorType != ColorType.RGBA_8888 &&
+            bitmap.colorType != ColorType.RGB_565 &&
+            bitmap.colorType != ColorType.RGBA_F16
+        ) {
+            error("Bitmap colorType (${bitmap.colorType}) is not supported.")
+        }
+
         val rgbImage = alloc<avifRGBImage>()
         avifRGBImageSetDefaults(rgbImage.ptr, avifImagePtr)
 
-        rgbImage.format = AVIF_RGB_FORMAT_RGBA
-        rgbImage.depth = 8u
+        when (bitmap.colorType) {
+            ColorType.RGBA_F16 -> {
+                rgbImage.depth = 16u
+                rgbImage.isFloat = AVIF_TRUE
+            }
+            ColorType.RGB_565 -> {
+                rgbImage.format = AVIF_RGB_FORMAT_RGB_565
+                rgbImage.depth = 8u
+            }
+            else -> {
+                rgbImage.format = AVIF_RGB_FORMAT_RGBA
+                rgbImage.depth = 8u
+            }
+        }
 
         var result = avifRGBImageAllocatePixels(rgbImage.ptr)
         if (result != AVIF_RESULT_OK) {
